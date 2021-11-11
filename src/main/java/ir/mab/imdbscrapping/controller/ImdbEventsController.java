df511e70ed8f6a2239cec014d7dffe79af0f1496
mabahmani <aminbahmani10@gmail.com>
2021-11-11 23:36:54 +0330

    fetch imdb all events

--- src/main/java/ir/mab/imdbscrapping/controller/ImdbEventsController.java
+++ src/main/java/ir/mab/imdbscrapping/controller/ImdbEventsController.java
@@ -2,8 +2,16 @@ package ir.mab.imdbscrapping.controller;
 
 import ir.mab.imdbscrapping.model.ApiResponse;
 import ir.mab.imdbscrapping.model.Event;
+import ir.mab.imdbscrapping.model.EventSummary;
 import ir.mab.imdbscrapping.model.News;
 import ir.mab.imdbscrapping.util.AppConstants;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -22,7 +31,6 @@ import java.util.regex.Pattern;
 @RestController
 @RequestMapping(path = AppConstants.Api.EVENTS)
 public class ImdbEventsController {
-    private final Pattern namePattern = Pattern.compile("nm+[0-9]+");
 
     @GetMapping("/{eventId}/{year}")
     ApiResponse<Event> fetchEventDetails(@PathVariable("eventId") String eventId,@PathVariable("year") String year) {
@@ -220,28 +228,37 @@ public class ImdbEventsController {
 
         return new ApiResponse<>(event, null, true);
     }
-   private String generateCover(String url, int width, int height) {
 
-        if (url.isEmpty())
-            return null;
+    @GetMapping("/")
+    ApiResponse<List<EventSummary>> fetchAllEvents() {
+        List<EventSummary> events = new ArrayList<>();
 
-        if (width == 0 || height == 0) {
-            String[] coverUrlSplits = url.split("._V1_");
-            return coverUrlSplits[0] + "._V1_.jpg";
+        try {
+            Document doc = Jsoup.connect("https://d2vyvawkvp20f9.cloudfront.net/event/index/data.json").ignoreContentType(true).get();
+
+            try {
+
+                JSONArray responseJson = new JSONArray(doc.body().text());
+                for (Object o : responseJson){
+                    try {
+                        JSONObject eventObject = (JSONObject) o;
+                        EventSummary eventSummary = new EventSummary();
+                        eventSummary.setEvent(eventObject.getString("name"));
+                        eventSummary.setEventId(eventObject.getString("id"));
+                        events.add(eventSummary);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }catch (Exception e){
+                return new ApiResponse<>(null, e.getMessage(), false);
+            }
+
+        } catch (IOException e) {
+            return new ApiResponse<>(null, e.getMessage(), false);
         }
 
-        String[] coverUrlSplits = url.split("._V1_");
-        String baseUrl = coverUrlSplits[0] + "._V1_";
-        String options = String.format("UY%s_CR%s,0,%s,%s_AL_.jpg", height, 0, 0, 0);
-        return baseUrl + options;
-    }
-
-    private String extractNameId(String text) {
-        Matcher m = namePattern.matcher(text);
-        if (m.find())
-            return m.group();
-
-        return null;
+        return new ApiResponse<>(events, null, true);
     }
 
 }
---
+++ src/main/java/ir/mab/imdbscrapping/model/EventSummary.java
@@ -0,0 +1,22 @@
+package ir.mab.imdbscrapping.model;
+
+public class EventSummary {
+    String event;
+    String eventId;
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+}
